82ed70dcb4eea87de0a906d24ab7174546774009
Md Hasnain <md.hasnain@switcom.de>
2020-05-26 09:46:18 +0200

    maps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,21 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
 import { TeamComponent } from './team/team.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { ClaintComponent } from './claint/claint.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+import { ImpressumComponent } from './impressum/impressum.component';
 
 
 const routes: Routes = [
-  // {path:'home', component:HomeComponent},
-  // {path:'about', component:AboutComponent},
-  // {path:'services', component:ServicesComponent},
-  // {path:'portfolio', component:PortfolioComponent},
-  // {path:'team', component:TeamComponent},
+  
+  {path:'', component:HomeComponent},
+  {path:'home', component:HomeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'services', component:ServicesComponent},
+  {path:'portfolio', component:PortfolioComponent},
+  {path:'team', component:TeamComponent},
   {path:'contact', component:ContactComponent},
 ];
 
@@ -22,4 +29,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [HomeComponent,ServicesComponent,PortfolioComponent,TeamComponent,AboutComponent, ContactComponent]
+export const routingComponents = [HomeComponent,ServicesComponent,PortfolioComponent,TeamComponent,AboutComponent,
+  ClaintComponent, ContactComponent, HeaderComponent, FooterComponent, SlideshowComponent, ImpressumComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+
   ],
   imports: [
     BrowserModule,
